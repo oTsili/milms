5ddc93ac4710f6f0a30d7c7c5c7772b3deb54207
odytsili <tsiligeridiso@gmail.com>
2021-06-19 15:35:11 -0400

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -19,9 +19,31 @@ export * from './middlewares/multipleFilesUpload';
 export * from './events/base/base-listener';
 export * from './events/base/base-publisher';
 export * from './events/base/subjects';
+
+export * from './events/assignment-materials/assignment-materials-created-event';
+export * from './events/assignment-materials/assignment-materials-deleted-event';
+export * from './events/assignment-materials/assignment-materials-updated-event';
+
 export * from './events/assignments/assignment-created-event';
 export * from './events/assignments/assignment-updated-event';
 export * from './events/assignments/assignment-deleted-event';
+
+export * from './events/course-materials/course-materials-created-event';
+export * from './events/course-materials/course-materials-updated-event';
+export * from './events/course-materials/course-materials-deleted-event';
+
+export * from './events/courses/course-created-event';
+export * from './events/courses/course-updated-event';
+export * from './events/courses/course-deleted-event';
+
+export * from './events/student-delivery-assignments/student-delivery-assignment-created-event';
+export * from './events/student-delivery-assignments/student-delivery-assignment-updated-event';
+export * from './events/student-delivery-assignments/student-delivery-assignment-deleted-event';
+
+export * from './events/student-delivery-files/student-delivery-file-created-event';
+export * from './events/student-delivery-files/student-delivery-file-updated-event';
+export * from './events/student-delivery-files/student-delivery-file-deleted-event';
+
 export * from './events/users/user-signup-event';
 export * from './events/users/user-signin-event';
 export * from './events/users/user-signout-event';
